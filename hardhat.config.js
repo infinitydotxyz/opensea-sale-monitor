dd3cf0d7b81cdc81fc1b6b028844514a21bf3fe3
Adi Kancherla <findadit@gmail.com>
2021-11-17 11:36:07 -0500

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,5 @@
-require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,14 +8,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
   networks: {
     mainnet: {
-      url: process.env.MAINNET_URL,
-      accounts: [process.env.PRIVATE_KEY]
-      // accounts: {
-      //   mnemonic: process.env.MAINNET_MNEMONIC
-      // }
+      url: process.env.MAINNET_URL
     }
   }
 };
--- scripts/monitor.js
+++ scripts/monitor.js
@@ -1,47 +1,44 @@
-const hre = require("hardhat");
-const OpenSeaABI = require('../OpenSeaABI.json')
-const OPENSEA_ADDRESS = "0x7be8076f4ea4a4ad08075c2508e481d6c946d12b"
+const { ethers } = require('ethers');
+const OpenSeaABI = require('../OpenSeaABI.json');
+const OPENSEA_ADDRESS = '0x7be8076f4ea4a4ad08075c2508e481d6c946d12b';
 const iface = new ethers.utils.Interface(OpenSeaABI);
-const provider = ethers.getDefaultProvider()
+const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_URL);
 
 async function main() {
-  const signers = await ethers.getSigners();
+  console.log('==== OpenSea Sale Monitor ====');
 
-  console.log("==== OpenSea Sale Monitor ====");
-  console.log(`Executing from ${signers[0].address}`)
+  const contract = new ethers.Contract(OPENSEA_ADDRESS, OpenSeaABI, provider);
 
-  const contract = new ethers.Contract(OPENSEA_ADDRESS, OpenSeaABI, signers[0]);
-
-  contract.on("OrdersMatched", async (...args) => {
+  contract.on('OrdersMatched', async (...args) => {
     const event = args[args.length - 1];
-    const response = (await provider.getTransaction(event.transactionHash)).data
-    const decodedResponse = iface.decodeFunctionData("atomicMatch_", response)
-    const collectionAddress = decodedResponse.addrs[4]
-    const seller = decodedResponse.addrs[8]
-    const buyer = decodedResponse.addrs[1]
-    const buyData = decodedResponse.calldataBuy
+    const response = (await provider.getTransaction(event.transactionHash)).data;
+    const decodedResponse = iface.decodeFunctionData('atomicMatch_', response);
+    const collectionAddress = decodedResponse.addrs[4];
+    const seller = decodedResponse.addrs[8];
+    const buyer = decodedResponse.addrs[1];
+    const buyData = decodedResponse.calldataBuy;
 
-    let tokenId, quantity
-
-    if (response.length > 4810) {
-      // ERC-1155 
-      tokenId = ethers.BigNumber.from("0x" + decodedResponse.calldataBuy.slice(138, 202)).toString()
-      quantity = ethers.BigNumber.from("0x" + decodedResponse.calldataBuy.slice(202, 266)).toString()
-    }
-    else {
-      // ERC-721
-      tokenId = ethers.BigNumber.from("0x" + buyData.slice(buyData.length - 32)).toString()
-    }
+    let tokenId, quantity;
 
     if (response.length > 4810) {
-      console.log(`{${event.transactionHash}} [${seller}] sold [${quantity}] of tokenID [${tokenId}] from ERC-1155 collection [${collectionAddress}] to [${buyer}]`)
-
+      // ERC-1155
+      tokenId = ethers.BigNumber.from('0x' + decodedResponse.calldataBuy.slice(138, 202)).toString();
+      quantity = ethers.BigNumber.from('0x' + decodedResponse.calldataBuy.slice(202, 266)).toString();
     } else {
-      console.log(`{${event.transactionHash}} [${seller}] sold tokenID [${tokenId}] from ERC-721 collection [${collectionAddress}] to [${buyer}]`)
+      // ERC-721
+      tokenId = ethers.BigNumber.from('0x' + buyData.slice(buyData.length - 32)).toString();
     }
 
+    if (response.length > 4810) {
+      console.log(
+        `{${event.transactionHash}} [${seller}] sold [${quantity}] of tokenID [${tokenId}] from ERC-1155 collection [${collectionAddress}] to [${buyer}]`
+      );
+    } else {
+      console.log(
+        `{${event.transactionHash}} [${seller}] sold tokenID [${tokenId}] from ERC-721 collection [${collectionAddress}] to [${buyer}]`
+      );
+    }
   });
-
 }
 
-main()
+main();
